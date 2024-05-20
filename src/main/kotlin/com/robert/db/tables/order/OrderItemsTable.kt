d8430042b92e586b3817e4262b226cbdd2521760
robert <robertnganga220@gmail.com>
2024-05-20 15:02:48 +0300

    Create order table

---
+++ src/main/kotlin/com/robert/db/tables/order/OrderItemsTable.kt
@@ -0,0 +1,13 @@
+package com.robert.db.tables.order
+
+import com.robert.db.tables.shoe.ShoesTable
+import org.jetbrains.exposed.sql.Table
+
+object OrderItemsTable : Table() {
+    val id = integer("id").autoIncrement()
+    val orderId = reference("order_id", OrdersTable.id)
+    val shoeId = reference("shoe_id", ShoesTable.id)
+    val quantity = integer("quantity")
+    val price = decimal("price", 10, 2)
+    override val primaryKey = PrimaryKey(id)
+}
---
+++ src/main/kotlin/com/robert/db/tables/order/OrdersTable.kt
@@ -0,0 +1,19 @@
+package com.robert.db.tables.order
+
+import com.robert.db.tables.cart.CartTable
+import com.robert.db.tables.user.UsersTable
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.javatime.CurrentDateTime
+import org.jetbrains.exposed.sql.javatime.datetime
+
+object OrdersTable : Table() {
+    val id = integer("id").autoIncrement()
+    val userId = reference("user_id", UsersTable.id)
+    val cartId = reference("cart_id", CartTable.id)
+    val total = decimal("total", 10, 2)
+    val status = varchar("status", 50)
+    val createdAt = datetime("created_at").defaultExpression(CurrentDateTime)
+    val updatedAt = datetime("updated_at").defaultExpression(CurrentDateTime)
+
+    override val primaryKey = PrimaryKey(id)
+}
